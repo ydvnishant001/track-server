272c21c86a37a5809976407af07da51ae7b47603
Nishant Yadav <yadav.nishant001@gmail.com>
2020-09-22 08:22:45 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri = "mongo url here";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo instance", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
